b00024297772d2bc022c2f2275cfaad68ce6a6a8
blue-velvet <31938637+blue-velvet@users.noreply.github.com>
2018-07-19 14:35:50 +0300

    first part completed

--- practice01.py
+++ practice01.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
@@ -9,19 +11,52 @@ def driver(request):
     path_g = r"C:\\Users\\kchumakov.000\\Documents\\drivers\\chromedriver.exe"
     binary_ff = r"C:\Users\kchumakov.000\AppData\Local\Mozilla Firefox\firefox.exe"
     binary_g = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-    wd = webdriver.Firefox(executable_path=path_ff, firefox_binary=binary_ff)
+    wd = webdriver.Chrome(executable_path=path_g)
+    wd.maximize_window()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
 
 def test_example(driver):
-    driver.get("https://www.octafx.com/")
-    WebDriverWait(driver, 2000)
-    driver.find_element_by_xpath("/html/body/main/section[1]/div[1]/div/div[1]/div[2]/a").click()
-    driver.find_element_by_name("firstName").send_keys("testName")
-    driver.find_element_by_name("lastName").send_keys("testLastName")
-    driver.find_element_by_xpath("/html/body/aside/div/div[3]/div[1]/form/div[2]/div/input").send_keys("testEmail@mail.com")
-    driver.find_element_by_xpath("/html/body/aside/div/div[3]/div[1]/form/div[1]/div[2]/div/input").send_keys("testPassword")
-    driver.find_element_by_class_name("/html/body/aside/div/div[3]/div[1]/form/div[4]/button/span").click()
-    WebDriverWait(driver, 1000)
+    name = "testNamea"
+    lastName = "testLastNamea"
+    email = "testEmaila@mail.com"
+    password = "123456"
+
+    driver.implicitly_wait(50)
+    driver.get("http://www.app32.appdevstage.com/")
+    driver.implicitly_wait(50)
+    driver.find_element_by_xpath("//a[@href='http://my.app32.appdevstage.com/open-account']").click()
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//aside")))
+    driver.find_element_by_xpath("//input[@name='firstName']").send_keys(name)
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "//input[@name='lastName']")))
+    driver.find_element_by_xpath("//input[@name='lastName']").send_keys(lastName)
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "/html/body/aside/div/div[3]/div[1]/form/div[2]/div/input")))
+    driver.find_element_by_xpath("/html/body/aside/div/div[3]/div[1]/form/div[2]/div/input").send_keys(email)
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "/html/body/aside/div/div[3]/div[1]/form/div[3]/div/input")))
+    driver.find_element_by_xpath("/html/body/aside/div/div[3]/div[1]/form/div[3]/div/input").send_keys(password)
+    driver.save_screenshot('screenie.png')
+    driver.find_element_by_xpath("//button[@data-auto-event-label='Open account']").click()
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "/html/body/aside/div/div[3]/div[2]/div[1]")))
+
+    driver.implicitly_wait(50)
+    driver.get("http://mailhog.app32.appdevstage.com/")
+    driver.find_element_by_xpath("//input[@ng-model='searchText'").send_keys(email)
+    driver.find_element_by_class_name("subject unread ng-binding").click()
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (By.PARTIAL_LINK_TEXT, "http://my.app32.appdevstage.com/verification/email/")))
+    driver.find_element_by_partial_link_text("http://my.app32.appdevstage.com/verification/email/").click()
+
+
